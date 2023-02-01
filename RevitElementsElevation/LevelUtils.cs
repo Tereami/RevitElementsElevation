923966b936201f5574076da590006ee09ad6088f
alter-sunset <87358040+alter-sunset@users.noreply.github.com>
2023-02-01 18:48:49 +0300

    Update LevelUtils.cs
    Added 0 value check at line 197.
    
    It seems that when you are using Generic Models, you actually have BuiltInParameter.INSTANCE_FREE_HOST_OFFSET_PARAM
    that actually has value near 0.
    But what we really seek in this case is BuiltInParameter.INSTANCE_ELEVATION_PARAM
    but it is skipped, because we already got 0 value from previous Parameter.

--- RevitElementsElevation/LevelUtils.cs
+++ RevitElementsElevation/LevelUtils.cs
@@ -194,7 +194,7 @@ namespace RevitElementsElevation
             foreach (BuiltInParameter bip in builtInParameters)
             {
                 param = elem.get_Parameter(bip);
-                if (param != null && param.HasValue)
+                if (param != null && param.HasValue && (int)param.AsDouble() != 0)
                 {
                     double elev = param.AsDouble();
                     return elev;
